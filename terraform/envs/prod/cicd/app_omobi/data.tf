8ed1321b9fdb4abaead6f1b6f15c825165eac065
kenta1996 <1234567890kentatomo@gmail.com>
2022-10-12 20:07:58 +0900

    add

--- terraform/envs/prod/cicd/app_omobi/data.tf
+++ terraform/envs/prod/cicd/app_omobi/data.tf
@@ -2,7 +2,3 @@ data "aws_ecs_service" "this" {
   cluster_arn  = "${local.name_prefix}-${local.service_name}"
   service_name = "${local.name_prefix}-${local.service_name}"
 }
-
-data "aws_s3_bucket" "env_file" {
-  bucket = "terraform-${local.name_prefix}-${local.service_name}-env-file"
-}
