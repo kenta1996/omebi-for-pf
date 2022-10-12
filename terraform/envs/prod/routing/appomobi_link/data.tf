4a49844520b0f7ea6788e6e9aff35195af9b4440
kenta1996 <1234567890kentatomo@gmail.com>
2022-10-12 10:38:18 +0900

    albの設定、route53のAレコード作成#18

---
+++ terraform/envs/prod/routing/appomobi_link/data.tf
@@ -0,0 +1,20 @@
+# terraform_remote_stateで、tfstate内のputputを参照できる
+data "terraform_remote_state" "network_main" {
+  backend = "s3"
+
+  config = {
+    bucket = "omobi-tfstate"
+    key    = "${local.system_name}/${local.env_name}/network/main_v1.2.8.tfstate"
+    region = "ap-northeast-1"
+  }
+}
+
+data "terraform_remote_state" "log_alb" {
+  backend = "s3"
+
+  config = {
+    bucket = "omobi-tfstate"
+    key    = "${local.system_name}/${local.env_name}/log/alb_v1.2.8.tfstate"
+    region = "ap-northeast-1"
+  }
+}
