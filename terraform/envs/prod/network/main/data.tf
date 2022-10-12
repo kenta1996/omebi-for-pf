13b6a846e63499a3206e33c7d35cf2799a128146
kenta1996 <1234567890kentatomo@gmail.com>
2022-10-12 09:45:08 +0900

    ネットワーク関連の設定#18

---
+++ terraform/envs/prod/network/main/data.tf
@@ -0,0 +1,2 @@
+# リージョン名の取得
+data "aws_region" "current" {}
---
+++ terraform/envs/prod/network/main/variables.tf
@@ -0,0 +1,23 @@
+# vpcのCIDR
+variable "vpc_cidr" {
+  type    = string
+  default = "172.31.0.0/16"
+}
+
+# サブネットのCIDR
+variable "azs" {
+  type = map(object({
+    public_cidr  = string
+    private_cidr = string
+  }))
+  default = {
+    a = {
+      public_cidr  = "172.31.0.0/20"
+      private_cidr = "172.31.48.0/20"
+    },
+    c = {
+      public_cidr  = "172.31.16.0/20"
+      private_cidr = "172.31.64.0/20"
+    }
+  }
+}
